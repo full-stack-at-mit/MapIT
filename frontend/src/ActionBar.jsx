15de0e6982efe383a9f457c30f95d1f17883644c
Hailey <haileyrpan@gmail.com>
2024-10-27 19:22:06 -0400

    different tabs

--- frontend/src/ActionBar.jsx
+++ frontend/src/ActionBar.jsx
@@ -12,18 +12,38 @@ import {
   faChevronRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-const ActionBar = ({ toggleSidebar, isOpen }) => {
+const ActionBar = ({ toggleSidebar, isOpen, onPageChange }) => {
   return (
     <div className="flex flex-col items-center space-y-5 bg-gray-100 p-2 rounded-lg shadow-md">
-      <FontAwesomeIcon icon={faUser} className="w-8 h-8 text-gray-500" />
-      <FontAwesomeIcon icon={faSearch} className="w-8 h-8 text-gray-500" />
-      <FontAwesomeIcon icon={faBookmark} className="w-8 h-8 text-gray-500" />
-      <FontAwesomeIcon icon={faPlus} className="w-8 h-8 text-gray-500" />
-      <FontAwesomeIcon icon={faUtensils} className="w-8 h-8 text-gray-500" />
-      <FontAwesomeIcon icon={faBook} className="w-8 h-8 text-gray-500" />
       <FontAwesomeIcon
-        icon={faChalkboardTeacher}
-        className="w-8 h-8 text-gray-500"
+        icon={faUser}
+        className="w-8 h-8 text-gray-500 cursor-pointer"
+        onClick={() => onPageChange("profile")}
+      />
+      <FontAwesomeIcon
+        icon={faSearch}
+        className="w-8 h-8 text-gray-500 cursor-pointer"
+        onClick={() => onPageChange("search")}
+      />
+      <FontAwesomeIcon
+        icon={faBookmark}
+        className="w-8 h-8 text-gray-500 cursor-pointer"
+        onClick={() => onPageChange("bookmarks")}
+      />
+      <FontAwesomeIcon
+        icon={faPlus}
+        className="w-8 h-8 text-gray-500 cursor-pointer"
+        onClick={() => onPageChange("add")}
+      />
+      <FontAwesomeIcon
+        icon={faUtensils}
+        className="w-8 h-8 text-gray-500 cursor-pointer"
+        onClick={() => onPageChange("dining")}
+      />
+      <FontAwesomeIcon
+        icon={faBook}
+        className="w-8 h-8 text-gray-500 cursor-pointer"
+        onClick={() => onPageChange("classes")}
       />
       <div className="w-full border-t border-gray-300 my-4"></div>
 
