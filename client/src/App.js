421f0e93fecd247ae36b7ed8be350b29b96c45a9
Hailey <haileyrpan@gmail.com>
2024-11-13 10:52:46 -0500

    refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,66 +1,48 @@
-// import {
-//   BrowserRouter,
-//   Navigate,
-//   Routes,
-//   Route,
-//   Outlet,
-// } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
 
-// import Home from "./pages/Home.js";
-// import Dashboard from "./pages/Dashboard.js";
-// import Register from "./pages/Register.js";
-// import Login from "./pages/Login.js";
-// import { useSelector } from "react-redux";
+import Home from "./pages/Home.js";
+import Dashboard from "./pages/Dashboard.js";
+import Register from "./pages/Register.js";
+import Login from "./pages/Login.js";
+import { useSelector } from "react-redux";
 
-// const PrivateRoutes = () => {
-//   const { isAuth } = useSelector((state) => state.auth);
-//   // check if user is authenticated. if so, render outlet (content for the page)
-//   return <>{isAuth ? <Outlet /> : <Navigate to="/login" />}</>;
-// };
-
-// const RestrictedRoutes = () => {
-//   const { isAuth } = useSelector((state) => state.auth);
-
-//   return <>{!isAuth ? <Outlet /> : <Navigate to="/dashboard" />}</>;
-// };
-
-// const App = () => {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-
-//         <Route element={<PrivateRoutes />}>
-//           <Route path="/dashboard" element={<Dashboard />} />
-//         </Route>
-
-//         <Route element={<RestrictedRoutes />}>
-//           <Route path="/register" element={<Register />} />
-//           <Route path="/login" element={<Login />} />
-//         </Route>
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// };
-
-// export default App;
-import React, { useState } from "react";
-import Map from "./pages/Map";
-import Sidebar from "./components/Sidebar";
+import React from "react";
 import './index.css'
 
 function App() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const PrivateRoutes = () => {
+    const { isAuth } = useSelector((state) => state.auth);
+    // check if user is authenticated. if so, render outlet (content for the page)
+    return <>{isAuth ? <Outlet /> : <Navigate to="/login" />}</>;
+  };
 
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
+  const RestrictedRoutes = () => {
+    const { isAuth } = useSelector((state) => state.auth);
+
+    return <>{!isAuth ? <Outlet /> : <Navigate to="/dashboard" />}</>;
   };
 
   return (
-    <div className="relative h-screen w-screen">
-      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <Map />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route element={<PrivateRoutes />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+
+        <Route element={<RestrictedRoutes />}>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,22 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import { NavLink } from "react-router-dom";
-import "../index.css"
+import React, { useState } from "react";
+import "../index.css";
+import Sidebar from "../components/Sidebar";
+import Map from "./Map";
 
 const Home = () => {
-  return (
-    <Layout>
-      <div className="container text-center my-5">
-      <h1 className=''>MIT Campus Map</h1>
-      <div className="mt-5">
-        <h3>Get Started</h3>
-        <p>Explore the links below:</p>
-        <div className="d-flex justify-content-center gap-3 mt-3">
-          {/* <Link href="/about" className="btn btn-outline-primary">About Us</Link> */}
-          <NavLink to="/register" className="btn btn-primary mr-3">Sign Up</NavLink>
-          <NavLink to="/login" className="btn btn-primary">Log in</NavLink>
-        </div>
-      </div>
-    </div>
-    </Layout>
-  )
-}
 
-export default Home
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className="relative h-screen w-screen">
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <Map />
+    </div>
+  );
+};
+
+export default Home;
